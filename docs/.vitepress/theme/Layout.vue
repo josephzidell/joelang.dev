6463093dbc8399c48f4e758311ae92ce12824900
Joe Zidell <joe.zidell@gmail.com>
2023-05-04 21:30:13 -0400

    Adds a version badge

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme';
+
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <Layout>
+    <template #nav-bar-title-after>
+      <img src="https://shields.io/github/v/release/josephzidell/joelang?display_name=tag&include_prereleases&sort=semver" alt="Latest Release" />
+    </template>
+  </Layout>
+</template>
